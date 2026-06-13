e44d7a7587dcf4ef1d67ced826ccc0f880ba3a68
Sai <saladisai555@gmail.com>
2026-06-13 13:24:52 +0530

    updated product_demo(2nd question)

--- src/main/java/com/sai/jpa/app/ProductDemo.java
+++ src/main/java/com/sai/jpa/app/ProductDemo.java
@@ -3,5 +3,20 @@ import jakarta.persistence.*;
 import com.sai.jpa.util.JpaUtil;
 import com.sai.jpa.entity.Product;
 public class ProductDemo {
+     public static void main(String[] args){
+         EntityManager em=JpaUtil.getEntityManager();
+         EntityTransaction tx=em.getTransaction();
+         Product product=new Product(1,"sai",1000);
+         try{
+             tx.begin();
+             em.persist(product);
+             Product p1=new Product(2,"gireesh",2000);
+             em.persist(p1);
+             tx.commit();
+             em.close();
+         } catch (Exception e) {
+             tx.rollback();
+         }
+     }
 
 }
